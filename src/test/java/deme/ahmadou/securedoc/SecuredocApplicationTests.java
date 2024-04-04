ca027032239ad5cd04cb2730cae036df6f05ae4e
bambademe <cheikhbambademe@gmail.com>
2024-04-04 01:49:37 +0000

    init project

---
+++ src/test/java/deme/ahmadou/securedoc/SecuredocApplicationTests.java
@@ -0,0 +1,13 @@
+package deme.ahmadou.securedoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
